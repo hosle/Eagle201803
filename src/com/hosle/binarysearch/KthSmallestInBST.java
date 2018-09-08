99203f0aac1f9d637189d7dfccd19be4df1d7316
hosle <hosle@163.com>
2018-09-09 00:00:36 +0800

    kth smallest in bst

--- src/com/hosle/binarysearch/KthSmallestInBST.java
+++ src/com/hosle/binarysearch/KthSmallestInBST.java
@@ -2,6 +2,8 @@ package com.hosle.binarysearch;
 
 import com.hosle.tree.TreeNode;
 
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Int;
+
 /**
  * Given a binary search tree,
  * write a function kthSmallest to find the kth smallest element in it.
@@ -31,9 +33,29 @@ import com.hosle.tree.TreeNode;
 
 public class KthSmallestInBST {
 
+    private int kthSmallestValue = Integer.MIN_VALUE;
+
     public int solution(TreeNode root, int k){
 
+        traverseTree(root,k, 0);
 
-        return 0;
+        return kthSmallestValue;
+    }
+
+    private int traverseTree(TreeNode root,int k, int count){
+        if (null == root) {
+            return 0;
+        }
+
+        int smallerCount = traverseTree(root.left, k, count);
+
+        if (smallerCount + 1 + count == k) {
+            kthSmallestValue = root.val;
+            return smallerCount + 1;
+        }
+
+        int largerCount = traverseTree(root.right, k, count + smallerCount + 1);
+
+        return smallerCount + largerCount + 1;
     }
 }
--- src/test/com/hosle/binarysearch/KthSmallestInBSTTest.java
+++ src/test/com/hosle/binarysearch/KthSmallestInBSTTest.java
@@ -1,5 +1,31 @@
+package com.hosle.binarysearch;
+
+import com.hosle.tree.TreeNode;
+import com.hosle.tree.TreeUtil;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class KthSmallestInBSTTest {
 
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void solution() {
+
+        Integer[] data = new Integer[]{3,1,4,null,2};
+
+        TreeNode root = new TreeUtil().createByArray(data);
+
+        assertEquals(2,new KthSmallestInBST().solution(root,2));
+        assertEquals(3,new KthSmallestInBST().solution(root,3));
+    }
 }
